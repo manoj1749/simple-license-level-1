eeccedcd6a50bfa0cbec597401df80bf08ed947e
Manoj <manojparasuram.sadhanala@gmail.com>
2022-10-31 18:21:25 +0530

    added docker files

---
+++ server/server.py
@@ -0,0 +1,24 @@
+import socket
+import os
+
+HOST = "0.0.0.0"
+PORT = 8080
+hash = 242
+
+flag = (os.environ.get('chall_flag', "cyberchaze{anotheronebitesthedust}")).encode()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+        try:
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                data = conn.recv(1)
+                print(data[0]);
+                if data[0] == hash:
+                    conn.sendall(flag)
+                conn.close()
+        except Exception as e:
+            print(e)
+            continue
