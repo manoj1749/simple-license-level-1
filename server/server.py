1b326faa3de0a3d6d4908d2d6be1e15e5a2a155f
Manoj <manojparasuram.sadhanala@gmail.com>
2022-11-02 21:09:43 +0530

    updated server

--- server/server.py
+++ server/server.py
@@ -14,11 +14,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             conn, addr = s.accept()
             with conn:
                 print(f"Connected by {addr}")
-                data = conn.recv(1)
-                print(data[0])
-                if data[0] == 254:
-                    print("Correct")
-                    print(flag)
+                data = conn.recv(2048)
+                encData=data.decode("utf-8")
+                if encData[0: len(encData)-1] == "a9402a2e54a884a873945ee2bf38cb7":
                     conn.sendall(flag)
                 conn.close()
         except Exception as e:
